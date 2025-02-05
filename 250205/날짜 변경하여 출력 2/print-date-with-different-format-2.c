1d9d6cf49be44b93b084b01c65031066d344dead
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-05 15:34:49 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250205/날짜 변경하여 출력 2/print-date-with-different-format-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int y,m,d;
+
+    scanf("%d-%d-%d",&m,&d,&y);
+    printf("%d.%d.%d",y,m,d);
+    
+    return 0;
+    
+}
