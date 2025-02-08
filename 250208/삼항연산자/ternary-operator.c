f8f16e9d7b5757251ce5507cc46f3ebe89e57849
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-08 17:56:03 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250208/삼항연산자/ternary-operator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+
+    if(a==100){
+        printf("pass");
+    }
+    else{
+        printf("failure");
+    }
+    return 0;
+}
