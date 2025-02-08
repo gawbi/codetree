de0d7e92609224febf0c9ddc4d7bd91ce4ad4efa
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-08 15:50:44 +0900

    Title: 합과 평균의 차, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250208/합과 평균의 차/sub-of-average-and-sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b,c,sum;
+    sum=a+b+c;
+    scanf("%d %d %d",&a,&b,&c);
+    sum=a+b+c;
+    printf("%d\n%d\n%d",sum,(sum)/3,sum-(sum)/3);
+    return 0;
+}
