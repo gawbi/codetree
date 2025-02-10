be8de4231eda7bace11c5ca72b8389a668762958
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-10 10:55:11 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250210/시력 검사/eye-test.c
+++ 250210/시력 검사/eye-test.c
@@ -2,5 +2,16 @@
 
 int main() {
     // Please write your code here.
+    float a,b;
+    scanf("%f %f",&a,&b);
+    if (a>=1.0 && b>=1.0){
+        printf("High");
+    }
+    else if(a>=0.5 && b>=0.5){
+        printf("Middle");
+    }
+    else{
+        printf("Low");
+    }
     return 0;
 }
