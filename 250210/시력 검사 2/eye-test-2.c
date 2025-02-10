a5d780593228465be544090b9e82617ad10cddce
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-10 09:27:03 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250210/시력 검사 2/eye-test-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    float a;
+    scanf("%f",&a);
+
+    if(a>=1.0){
+        printf("High");
+    }
+    else if(a>=0.5){
+        printf("Middle");
+    }
+    else{
+        printf("Low");
+    }
+    
+    return 0;
+}
