71c6479ef8f8c743a5fb39866dc09fb6a47936e6
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-10 09:24:51 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250210/물의 상태/state-of-water.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+
+    if(a<0){
+        printf("ice");
+    }
+    else if(a>=100){
+        printf("vapor");
+    }
+    else{
+        printf("water");
+    }
+    return 0;
+}
