14af6a6a9e92d95b35980249161794272449936c
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-11 10:41:47 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250211/두 사람/two-person.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int age1,age2;
+    char sex1 ,sex2;
+
+    scanf("%d %c",&age1,&sex1);
+    scanf("%d %c",&age2,&sex2);
+    
+    if(age1>=19 && sex1=='M' || age2>=19 && sex2=='M'){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+
+    return 0;
+}
