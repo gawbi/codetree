89461213423efabbbdef602d83c15b2a1e446bd3
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-11 10:56:34 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250211/윤년인가/is-leap-year.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int y;
+    scanf("%d",&y);
+    if(y%4==0 && y%100==0 && (y%400!=0 )){
+        printf("true");
+    }
+    else{
+        printf("false")
+    }
+    return 0;
+
+}
