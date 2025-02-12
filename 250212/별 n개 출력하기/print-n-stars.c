4b9fdcd98d4b15092c6655252eab923fdadf9d6e
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-12 15:42:44 +0900

    Title: 별 n개 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250212/별 n개 출력하기/print-n-stars.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+    int i=1;
+    while(i<=n){
+        printf("*\n");
+        i++;
+    }
+    return 0;
+}
