4c4328d39300127b08eedcf29c5723e1883b09b5
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-12 17:17:15 +0900

    Title: n부터 1까지 정수 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250212/n부터 1까지 정수 출력/print-n-to-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+    
+    int i=n;
+    
+    while(i>=1){
+        printf("%d ",i);
+        i--;
+    }
+    return 0;
+}
