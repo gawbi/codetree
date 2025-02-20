bcefefcd8e5627911676cde2a46561550436ee6a
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-20 16:04:11 +0900

    Title: 1부터의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250220/1부터의 합/sum-from-the-one.c
+++ 250220/1부터의 합/sum-from-the-one.c
@@ -4,17 +4,17 @@ int main() {
     // Please write your code here.
     int n;
     scanf("%d",&n);
-    int sum=1;
+    int sum=0;
+    
     for(int i=1; i<=100; i++){
+        sum+=i;
         if(sum>=n){
             
         
             printf("%d",i);
             break;
         }
-        else{
-            sum+=i;
-        }
+       
     }
 
     return 0;
