34f8f2213e14772c2d7d6e9ec542bbf2fad8d4ab
gawbi <70633886+gawbi@users.noreply.github.com>
2025-02-25 16:39:36 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250225/직각 삼각형 출력/Right-triangle-and-output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    int j;
+
+    for(int i=1; i<=n; i++){
+        j =(i*2)-1;
+        for(j; j>0; j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
